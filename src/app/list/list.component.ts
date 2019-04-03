9d182852c83deca0fce0a8b5ad2652d32b6e2ad3
Zijian Xu <xuzijian@g.ecc.u-tokyo.ac.jp>
2019-04-03 12:56:20 +0900

    Parse github api response

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-list',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.less']
 })
 export class ListComponent implements OnInit {
+  public algorithms = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private http: HttpClient,
+    private sanitizer: DomSanitizer
+  ) {
+    this.getProgramUrlsSuccess = this.getProgramUrlsSuccess.bind(this);
+    this.error = this.error.bind(this);
   }
 
+  ngOnInit() {
+    this.getProgramUrls();
+  }
+
+  getProgramUrls() {
+    const apiUrl = 'https://api.github.com/repos/xuzijian629/library/contents';
+    let get = this.http.get(apiUrl);
+    get.subscribe(this.getProgramUrlsSuccess, this.error);
+  }
+
+  getProgramUrlsSuccess(response) {
+    for (let file of response) {
+      if (file['name'].match(/.cpp$/)) {
+        let name = file['name'].match(/(.*).cpp$/)[1]
+        this.algorithms.push(name);
+      }
+    }
+  }
+
+  error(error) {
+    console.error(error);
+  }
 }
