3a44cc9588460ff8f496ca9c44a47ffee7c620d2
Zijian Xu <xuzijian@g.ecc.u-tokyo.ac.jp>
2019-04-27 00:18:04 +0900

    Style algorithm-detail

--- src/app/algorithm-detail/algorithm-detail.component.ts
+++ src/app/algorithm-detail/algorithm-detail.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit, SecurityContext } from '@angular/core';
+import { Component, OnInit, SecurityContext, ViewEncapsulation } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 import { DomSanitizer, SafeScript, SafeValue } from '@angular/platform-browser';
+import * as marked from 'marked';
 
 @Component({
   selector: 'app-algorithm-detail',
   templateUrl: './algorithm-detail.component.html',
-  styleUrls: ['./algorithm-detail.component.less']
+  styleUrls: ['./algorithm-detail.component.less'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AlgorithmDetailComponent implements OnInit {
   private group: string;
   private name: string;
   public code: SafeValue;
+  private data: {[index: string]: string};
+  public comment: SafeHtml;
 
   constructor(
     private route: ActivatedRoute,
@@ -19,11 +23,13 @@ export class AlgorithmDetailComponent implements OnInit {
     private sanitizer: DomSanitizer
   ) {
     this.showDetailSuccess = this.showDetailSuccess.bind(this);
+    this.getProgramCommentsSuccess = this.getProgramCommentsSuccess.bind(this);
     this.error = this.error.bind(this);
     route.params.subscribe(params => {
       this.group = params['group'];
       this.name = params['algorithm'];
       this.showDetail();
+      this.getProgramComments();
     });
   }
 
@@ -36,12 +42,46 @@ export class AlgorithmDetailComponent implements OnInit {
     get.subscribe(this.showDetailSuccess, this.error);
   }
 
+  getProgramComments() {
+    const url = `https://raw.githubusercontent.com/xuzijian629/library/master/${this.group}/readme.md`;
+    let get = this.http.get(url, { responseType: 'text' });
+    get.subscribe(this.getProgramCommentsSuccess, this.error);
+  }
+
   showDetailSuccess(response) {
     this.code = this.sanitizer.sanitize(SecurityContext.NONE, response);
     setTimeout(PR.prettyPrint, 100);
   }
 
+  getProgramCommentsSuccess(response) {
+    this.data = this.parseProgramComments(response);
+    this.comment = this.sanitizer.bypassSecurityTrustHtml(marked(this.data[this.name]));
+  }
+
   error(error) {
     console.error(error);
+  }
+
+  parseProgramComments(s: string) {
+    let ret = {};
+    let tmp = "";
+    let title = "";
+    for (let line of s.split('\n')) {
+      if (line.length == 0) continue;
+      let match = line.match(/^# (\S+)$/);
+      if (match) {
+        if (title.length) {
+          ret[title] = tmp;
+          tmp = "";
+        }
+        title = match[1];
+      } else {
+        tmp += line + '\n';
+      }
+    }
+    if (title.length) {
+      ret[title] = tmp;
+    }
+    return ret;
   }
 }
